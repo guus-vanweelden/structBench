4b7bd459a1f39a2ce0d8048eccd3b725028e9f63
Guus van Weelden <guus.vanweelden@ligatus.com>
2016-11-25 12:36:57 +0000

    add Load benches

--- struct_test.go
+++ struct_test.go
@@ -5,8 +5,25 @@ import "testing"
 func benchmarkInsertStruct(s saverLoader, b *testing.B) {
 	d := generateItems(b.N)
 	b.ResetTimer()
+	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
 		s.Save(d[n])
+	}
+}
+
+func benchmarkLoadStruct(s saverLoader, b *testing.B) {
+	d := generateItems(b.N)
+	k := make([]key, len(d))
+	for n := 0; n < b.N; n++ {
+		if n%2 == 0 {
+			s.Save(d[n])
+		}
+		k[n] = d[n].Key()
+	}
+	b.ResetTimer()
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		s.Load(k[n])
 	}
 }
 
@@ -35,4 +52,31 @@ func BenchmarkSaveStruct_sliceMapString(b *testing.B) {
 func BenchmarkSaveStruct_mapStuct(b *testing.B) {
 	m := make(mapStruct)
 	benchmarkInsertStruct(m, b)
+}
+
+func BenchmarkLoadStruct_mapStringBool(b *testing.B) {
+	m := make(mapStringBool)
+	benchmarkLoadStruct(m, b)
+}
+
+func BenchmarkLoadStruct_mapBoolString(b *testing.B) {
+	m := make(mapBoolString)
+	benchmarkLoadStruct(m, b)
+}
+
+func BenchmarkLoadStruct_mapStringSlice(b *testing.B) {
+	m := make(mapStringSlice)
+	benchmarkLoadStruct(m, b)
+}
+
+func BenchmarkLoadStruct_sliceMapString(b *testing.B) {
+	m := sliceMapString{}
+	m[0] = make(map[string]*item)
+	m[1] = make(map[string]*item)
+	benchmarkLoadStruct(m, b)
+}
+
+func BenchmarkLoadStruct_mapStuct(b *testing.B) {
+	m := make(mapStruct)
+	benchmarkLoadStruct(m, b)
 }
